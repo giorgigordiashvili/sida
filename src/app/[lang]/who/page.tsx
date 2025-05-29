c6e2f46df7fb0d92b5b5b7a4840d9d7dd719288d
spaceehinata <nata.tediasvhili.1@btu.edu.ge>
2025-05-29 22:56:16 +0400

    this is just the beginning of who we are

--- src/app/[lang]/who/page.tsx
+++ src/app/[lang]/who/page.tsx
@@ -1,9 +1,8 @@
 import { getDictionary } from '@/get-dictionary';
 import { Locale } from '@/i18n-config';
 import HeroTheme from '@/components/HeroTheme';
-import DonationForm from '@/components/DonationForm';
 
-export default async function DoantionDetails(props: { params: Promise<{ lang: Locale }> }) {
+export default async function Who(props: { params: Promise<{ lang: Locale }> }) {
   const { lang } = await props.params;
 
   const dictionary = await getDictionary(lang);
@@ -13,7 +12,6 @@ export default async function DoantionDetails(props: { params: Promise<{ lang: L
         dictionary={dictionary.donationDetails}
         image="/assets/images/volunteer/volunteerHero.png"
       />
-      <DonationForm dictionary={dictionary.donationDetails} />
     </>
   );
 }
